2937c0acf9f9550c29c13e1b27f5b24ca429c65c
Lamouett3 <d.aitmessaoud@gmail.com>
2022-10-03 22:26:44 +0200

    work on index

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,5 @@
 // Récuperation des fichier du DOM
+
 const produits = function (produitsRecuperer) {
   console.log(produitsRecuperer);
 };
@@ -11,26 +12,25 @@ fetch("http://localhost:3000/api/products")
     produits(produitsRecuperer);
   });
 
-/*
 // Creation des element des produits
 
+const produitsIndex = produits;
+
 const imageElement = document.createElement("img"); // Image de l'element
-imageElement.src = "http://127.0.0.1:5500/back/images/kanap01.jpeg";
+imageElement.src = produitsIndex.imageURL;
 
 const nomElement = document.createElement("h3"); // titre de l'element
-nomElement.innerText = "Kanap Sinopé";
+nomElement.innerText = produitsIndex.name;
 
 const descriptionElement = document.createElement("p"); // description de l'element
-descriptionElement.innerText =
-  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+descriptionElement.innerText = produitsIndex.description;
 
 // Récuperartion des element du DOM
 
-const mesElements = document.querySelector(".items");
+const sectionItem = document.querySelector(".items");
 
 // Insertion des element dans la balise items
 
-mesElements.appendChild(imageElement);
-mesElements.appendChild(nomElement);
-mesElements.appendChild(descriptionElement);
-*/
+sectionItem.appendChild(imageElement);
+sectionItem.appendChild(nomElement);
+sectionItem.appendChild(descriptionElement);
