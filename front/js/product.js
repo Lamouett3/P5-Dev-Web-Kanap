7dfe3641daf75a7c7ee1c90b5989db03de0bba68
Lamouett3 <d.aitmessaoud@gmail.com>
2022-10-11 17:03:24 +0200

    work on url searchparams to get id

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,16 @@
 // Récuperation de la chaîne de requete dans l'url
 const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
+//console.log(queryString_url_id);
+
+/*    // methode 1 pour rextraire l'id avec slice
+const get_id = queryString_url_id.slice(1);
+console.log(get_id);         */
+
+// methode 2 pour rextraire l'id (URLSearchParams)
+const get_id = new URLSearchParams(queryString_url_id);
+//console.log(get_id);
+const id = get_id.get("id");
+console.log(id);
 
 /* RESSOURCES
 // penser a refaire un fetch (urlsearchparam)en fonction de l'id
