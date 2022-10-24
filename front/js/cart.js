9800a55c59c279e5181fcc0ae10a824e8a552624
Lamouett3 <d.aitmessaoud@gmail.com>
2022-10-25 01:21:15 +0200

    work on src card

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,63 +1,79 @@
 //------------------------La gestion panier-------------------------
+// Récuperer les données dans l'API HTTP
+const productApi = fetch("http://localhost:3000/api/products");
 
-// Récuperation des produits dans le local storage
-const productCart = localStorage.getItem("produitsPanier");
+// Affiche le tableau
+productApi.then((response) => {
+  // retourne les données au format JSON
+  const productApiJSON = response.json();
+  // affiche les données sous forme de array
+  productApiJSON.then((data) => {
+    // Récuperation des produits dans le local storage
+    const productCart = localStorage.getItem("produitsPanier");
 
-// convertie la string en object
-const productCartJSON = JSON.parse(productCart);
-console.log(productCartJSON);
+    // convertie la string en object
+    const productCartJSON = JSON.parse(productCart);
+    console.log(productCartJSON);
 
-// Création des contenaire
+    // Création des contenaire
 
-for (let i = 0; i < productCartJSON.length; i++) {
-  // id du produit
-  // const productName = productCartJSON[i].id_Produit;
-  // productName.innerText = productCartJSON[i].id_Produit;
+    for (let i = 0; i < productCartJSON.length; i++) {
+      // id du produit
+      // const productName = productCartJSON[i].id_Produit;
+      // productName.innerText = productCartJSON[i].id_Produit;
 
-  //-----------------CREATION DES CONTENAIRE ET DU CONTENU HTML
+      //-----------------CREATION DES CONTENAIRE ET DU CONTENU HTML
 
-  const productContainer = document.createElement("article");
-  productContainer.className = "cart__item";
-  console.log(productContainer);
+      const productContainer = document.createElement("article");
+      productContainer.className = "cart__item";
+      console.log(productContainer);
 
-  const productImage = document.createElement("div");
-  productImage.className = "cart__item__img";
-  // console.log(productImage);
+      const productImage = document.createElement("div");
+      productImage.className = "cart__item__img";
+      const productImageUrl = document.createElement("img");
+      productImageUrl.src = data[i].imageUrl;
+      // console.log(productImage);
 
-  const productContent = document.createElement("div");
-  productContent.className = "cart__item__content";
-  //console.log(productContent);
+      const productContent = document.createElement("div");
+      productContent.className = "cart__item__content";
+      //console.log(productContent);
 
-  const productDescription = document.createElement("div");
-  productDescription.className = "cart__item__content__description";
-  //console.log(productDescription);
+      const productDescription = document.createElement("div");
+      productDescription.className = "cart__item__content__description";
+      //console.log(productDescription);
+      const productDescriptionTitle = document.createElement("h2");
+      productDescription.innerText = data[i].name;
+      //console.log(productDescriptionTitle);
 
-  const productSetting = document.createElement("div");
-  productSetting.className = "cart__item__content__settings";
-  //console.log(productSetting);
-  const productQuantity = document.createElement("div");
-  productQuantity.className = "cart__item__content__settings__quantity";
-  //console.log(productQuantity);
+      const productSetting = document.createElement("div");
+      productSetting.className = "cart__item__content__settings";
+      //console.log(productSetting);
+      const productQuantity = document.createElement("div");
+      productQuantity.className = "cart__item__content__settings__quantity";
+      //console.log(productQuantity);
 
-  const productSettingDelete = document.createElement("div");
-  productSettingDelete.className = "cart__item__content__settings__delete";
-  //console.log(productSettingDelete);
+      const productSettingDelete = document.createElement("div");
+      productSettingDelete.className = "cart__item__content__settings__delete";
+      //console.log(productSettingDelete);
 
-  productContent.appendChild(productDescription);
-  productContent.appendChild(productSetting);
+      productImage.appendChild(productImageUrl);
+      productDescription.appendChild(productDescriptionTitle);
+      productContent.appendChild(productDescription);
+      productContent.appendChild(productSetting);
 
-  productSetting.appendChild(productQuantity);
-  productSetting.appendChild(productSettingDelete);
-  //console.log(productSetting);
+      productSetting.appendChild(productQuantity);
+      productSetting.appendChild(productSettingDelete);
+      //console.log(productSetting);
 
-  productContainer.appendChild(productImage);
-  productContainer.appendChild(productContent);
-  // productContainer.appendChild(productSetting);
+      productContainer.appendChild(productImage);
+      productContainer.appendChild(productContent);
+      // productContainer.appendChild(productSetting);
 
-  const sectionCart = document.getElementById(cart__items);
-  console.log(sectionCart);
-}
-
+      const sectionCart = document.getElementById(cart__items);
+      console.log(sectionCart);
+    }
+  });
+});
 // La récuperation des données séléctionnées par l'utilisateur et envoie du panier
 
 // Récuperer les élément dans le local storage
