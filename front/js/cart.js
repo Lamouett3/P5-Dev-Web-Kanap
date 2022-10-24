446a6d6ddfddd27508381dab219a5291944181a9
Lamouett3 <d.aitmessaoud@gmail.com>
2022-10-24 16:24:42 +0200

    modification of page cart and confirmation

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,55 +1,38 @@
-// Récuperer les données dans l'API HTTP
-const product = fetch("http://localhost:3000/api/products");
-// Affiche le tableau
-product.then((response) => {
-  // console.log(response);
-  // retourne les données au format JSON
-  const kanapData = response.json();
-  // affiche les données sous forme de array
-  // console.log(kanapData);
-  kanapData.then((data) => {
-    console.log(data);
-  });
-});
+// Création de la page panier
 
-/*--------------------RESSOURCES-----------------------------------------
+// Récuperer les élément dans le local storage
 
+// RESSOURCES BLOC INSERTION HTML
+/*
+<main class="limitedWidthBlockContainer">
+  <div class="cartAndFormContainer" id="cartAndFormContainer">
+          <h1>Votre panier</h1>
+          <section class="cart">
+            <section id="cart__items">
+             <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+                <div class="cart__item__img">
+                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>Nom du produit</h2>
+                    <p>Vert</p>
+                    <p>42,00 €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article> -->
+            </section>
+            <div class="cart__price">
+              <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
+            </div>
 
-  // Transformation des pieces en JSON
-  const panier = JSON.stringify(kanapData);
-  // Stockage des informations dans le localStorage
-  window.localStorage.setItem("kanapData", panier);
-  
-// Creation du tableau PANIER
-const panier = ["id", "QuantitéProduit", "CouleurProduit"];
-
-// création de la fonction "on click" du panier
-const boutonPanier = document.querySelector("#addToCart");
-boutonPanier.addEventListener("click", function () {
-  // Fonction qui ajoute une entrée dans le tableau panier
-});
-
-
-
- <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-<div class="cart__item__img">
-  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-</div>
-<div class="cart__item__content">
-  <div class="cart__item__content__description">
-    <h2>Nom du produit</h2>
-    <p>Vert</p>
-    <p>42,00 €</p>
-  </div>
-  <div class="cart__item__content__settings">
-    <div class="cart__item__content__settings__quantity">
-      <p>Qté : </p>
-      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-    </div>
-    <div class="cart__item__content__settings__delete">
-      <p class="deleteItem">Supprimer</p>
-    </div>
-  </div>
-</div>
-</article> 
 */
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,16 @@
 // Création de la page panier
+
+// Récuperer les élément dans le local storage
+
+// RESSOURCES BLOC INSERTION HTML
+/*
+<main class="limitedWidthBlockContainer">
+      <div class="limitedWidthBlock" id="limitedWidthBlock">
+        
+        <div class="confirmation">
+          <p>Commande validée ! <br>Votre numéro de commande est : <span id="orderId"><!-- 65431343444684674 --></span></p>
+        </div>
+
+      </div>
+    </main>
+*/
